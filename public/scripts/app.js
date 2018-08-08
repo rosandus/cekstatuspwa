7f3e9b4d65bd109822b5bfe10f8f850c3a2c6b24
Rosa Nur Rizky Febrianty Tamarindus <rosatamarindus@gmail.com>
2018-08-08 15:33:37 +0700

    Make code more readable

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,39 +6,49 @@
       // Connection Status
       document.getElementById('connectionstatus').innerHTML = navigator.onLine ? 'Online' : 'Offline';
 
+      // GPS
+      document.getElementById('gpsstatus').innerHTML = navigator.geolocation ? 'Yes' : 'No';
+      // checkGps();
+      // function checkGps() {
+      //     if ('geolocation' in navigator) {
+      //         document.querySelector('#gpsstatus').textContent = 'Yes';
+      //     } else {
+      //         document.querySelector('#gpsstatus').textContent = 'No';
+      //     }
+      // };
+
       // Rear Camera Status
-      checkCamera();
-      function checkCamera() {
-          if ('ImageCapture' in window) {
-              document.querySelector('#camerastatus').textContent = 'Yes';
-          } else {
-              document.querySelector('#camerastatus').textContent = 'No';
-          }
-      };
+      document.getElementById('camerastatus').innerHTML = window.ImageCapture ? 'Yes' : 'No';
+      // checkCamera();
+      // function checkCamera() {
+      //     if ('ImageCapture' in window) {
+      //         document.querySelector('#camerastatus').textContent = 'Yes';
+      //     } else {
+      //         document.querySelector('#camerastatus').textContent = 'No';
+      //     }
+      // };
 
       // Accelerometer Status
-      // document.getElementById('accestatus').innerHTML = navigator.onLine ? 'Online' : 'Offline';
-      if (window.Accelerometer == undefined) {
-          //No accelerometer is present. Use buttons.
-          document.getElementById('accestatus').innerHTML = 'No';
-      }
-      else {
-          document.getElementById('accestatus').innerHTML = 'Yes';
-          // window.addEventListener("devicemotion", accelerometerUpdate, true);
-      }
+      document.getElementById('accestatus').innerHTML = window.Accelerometer ? 'Yes' : 'No';
+      // if (window.Accelerometer == undefined) {
+      //     //No accelerometer is present. Use buttons.
+      //     document.getElementById('accestatus').innerHTML = 'No';
+      // }
+      // else {
+      //     document.getElementById('accestatus').innerHTML = 'Yes';
+      //     // window.addEventListener("devicemotion", accelerometerUpdate, true);
+      // }
 
       // Bluetooth Status
-      checkBluetooth();
-      function checkBluetooth() {
-          if ('bluetooth' in navigator) {
-              document.querySelector('#bluetoothstatus').textContent = 'Yes';
-          } else {
-              document.querySelector('#bluetoothstatus').textContent = 'No';
-          }
-      };
-      // if (!('bluetooth' in navigator)) {
-      //     $target.innerText = 'Bluetooth API not supported.';
-      //     return;
+      document.getElementById('bluetoothstatus').innerHTML = navigator.bluetooth ? 'Yes' : 'No';
+      // checkBluetooth();
+      // function checkBluetooth() {
+      //     if ('bluetooth' in navigator) {
+      //         document.querySelector('#bluetoothstatus').textContent = 'Yes';
+      //     } else {
+      //         document.querySelector('#bluetoothstatus').textContent = 'No';
+      //     }
+      // };
 
       // Battery Status
       function updateBatteryStatus(battery) {
@@ -83,7 +93,7 @@
       }
 
       // Front Camera
-      getStream();
+      // getStream();
       // function getUserMedia(options, successCallback, failureCallback) {
       //     var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
       //         navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -92,30 +102,25 @@
       //     }
       // }
 
-      function getStream() {
-          if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-              !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-              document.querySelector('#frontcamerastatus').textContent = 'No';
-              return;
-          } else {
-              document.querySelector('#frontcamerastatus').textContent = 'Yes';
-          }
-      }
-
-      // GPS
-      checkGps();
-      function checkGps() {
-          if ('geolocation' in navigator) {
-              document.querySelector('#gpsstatus').textContent = 'Yes';
-          } else {
-              document.querySelector('#gpsstatus').textContent = 'No';
-          }
-      };
+      // function getStream() {
+      //     if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+      //         !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+      //         document.querySelector('#frontcamerastatus').textContent = 'No';
+      //         return;
+      //     } else {
+      //         document.querySelector('#frontcamerastatus').textContent = 'Yes';
+      //     }
+      // }
 
       // Tap Status
       checkTap();
+      let isTouchCapable = 'ontouchstart' in window ||
+          window.DocumentTouch && document instanceof window.DocumentTouch ||
+          navigator.maxTouchPoints ||
+          window.navigator.msMaxTouchPoints;
       function checkTap() {
-          if ('ontouchstart' in window){
+          if (isTouchCapable){
+              document.addEventListener('touchstart', myTouchFunction, false);
               document.querySelector('#tapstatus').textContent = 'Yes';
           } else {
               document.querySelector('#tapstatus').textContent = 'No';

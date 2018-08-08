2d9aa9a2391334977f5737a0f424008241f6a234
Rosa Nur Rizky Febrianty Tamarindus <rosatamarindus@gmail.com>
2018-08-08 17:41:09 +0700

    Add Device OS Name

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -50,6 +50,79 @@
       //     }
       // };
 
+      // Device Name
+      checkDevice();
+      function checkDevice() {
+          'use strict';
+
+          let module = {
+              options: [],
+              header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+              dataos: [
+                  { name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+                  { name: 'Windows', value: 'Win', version: 'NT' },
+                  { name: 'iPhone', value: 'iPhone', version: 'OS' },
+                  { name: 'iPad', value: 'iPad', version: 'OS' },
+                  { name: 'Kindle', value: 'Silk', version: 'Silk' },
+                  { name: 'Android', value: 'Android', version: 'Android' },
+                  { name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+                  { name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+                  { name: 'Macintosh', value: 'Mac', version: 'OS X' },
+                  { name: 'Linux', value: 'Linux', version: 'rv' },
+                  { name: 'Palm', value: 'Palm', version: 'PalmOS' }
+              ],
+              init: function () {
+                  let agent = this.header.join(' '),
+                      os = this.matchItem(agent, this.dataos);
+                  return { os: os};
+              },
+              matchItem: function (string, data) {
+                  let i = 0,
+                      j = 0,
+                      html = '',
+                      regex,
+                      regexv,
+                      match,
+                      matches,
+                      version;
+
+                  for (i = 0; i < data.length; i += 1) {
+                      regex = new RegExp(data[i].value, 'i');
+                      match = regex.test(string);
+                      if (match) {
+                          regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+                          matches = string.match(regexv);
+                          version = '';
+                          if (matches) { if (matches[1]) { matches = matches[1]; } }
+                          if (matches) {
+                              matches = matches.split(/[._]+/);
+                              for (j = 0; j < matches.length; j += 1) {
+                                  if (j === 0) {
+                                      version += matches[j] + '.';
+                                  } else {
+                                      version += matches[j];
+                                  }
+                              }
+                          } else {
+                              version = '0';
+                          }
+                          return {
+                              name: data[i].name,
+                              version: parseFloat(version)
+                          };
+                      }
+                  }
+                  return { name: 'unknown', version: 0 };
+              }
+          };
+
+          const e = module.init();
+
+          document.getElementById('devicestatus').innerHTML = e.os.name + " " + e.os.version;
+          console.log(e.os.name);
+          console.log(e.os.version);
+      };
+
       // Battery Status
       function updateBatteryStatus(battery) {
           document.querySelector('#charging').textContent = battery.charging ? 'Charging' : 'Not Charging';
@@ -82,8 +155,8 @@
       }
 
       function updateNetworkInfo(info) {
-          let type = info.type.charAt(0).toUpperCase()+info.type.slice(1);
-          document.getElementById('connectivitytpe').innerHTML = type + "; " + info.effectiveType;
+          // let type = info.type.charAt(0).toUpperCase()+info.type.slice(1);
+          document.getElementById('connectivitytpe').innerHTML = info.type + "; " + info.effectiveType;
       }
 
       let info = getConnection();
@@ -114,10 +187,11 @@
 
       // Tap Status
       checkTap();
-      let isTouchCapable = 'ontouchstart' in window ||
-          window.DocumentTouch && document instanceof window.DocumentTouch ||
-          navigator.maxTouchPoints ||
-          window.navigator.msMaxTouchPoints;
+      // let isTouchCapable = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+      // let isTouchCapable = 'ontouchstart' in window ||
+      //     window.DocumentTouch && document instanceof window.DocumentTouch ||
+      //     navigator.maxTouchPoints ||
+      //     window.navigator.msMaxTouchPoints || 'onmsgesturechange' in window || window.navigator.msMaxTouchPoints;
       function checkTap() {
           if (isTouchCapable){
               document.addEventListener('touchstart', myTouchFunction, false);
